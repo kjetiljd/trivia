f7a978149470fed85cf2f711f41ce640340e1ead
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2021-02-08 10:08:54 +0100

    t Introduce ApprovalTest

--- kotlin/src/test/kotlin/com/adaptionsoft/games/trivia/SomeTest.kt
+++ kotlin/src/test/kotlin/com/adaptionsoft/games/trivia/SomeTest.kt
@@ -2,6 +2,7 @@ package com.adaptionsoft.games.trivia
 
 import com.adaptionsoft.games.trivia.runner.main
 import com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut
+import org.approvaltests.Approvals
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -24,6 +25,15 @@ class SomeTest {
             main(arrayOf(seed))
         }
         assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `Game runner output is as approved`() {
+        val actual = tapSystemOut {
+            val seed = "0"
+            main(arrayOf(seed))
+        }
+        Approvals.verify(actual)
     }
 }
 
